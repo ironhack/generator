eee3df404be141b1a4273f44ec304cf2ce12e3d9
Fer Martin <fer@ironhack.com>
2017-11-20 17:44:21 +0000

    finally everthing kinda working

--- templates/js/app.js
+++ templates/js/app.js
@@ -9,7 +9,14 @@ const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 
-{dbconnect}
+mongoose.Promise = Promise;
+mongoose
+  .connect('mongodb://localhost/{app_name}', {useMongoClient: true})
+  .then(() => {
+    console.log('Connected to Mongo!')
+  }).catch(err => {
+    console.error('Error connecting to mongo', err)
+  });
 
 const app = express();
 
@@ -23,9 +30,9 @@ app.use(cookieParser());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-{css}
+{expressCss}
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
