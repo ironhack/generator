6eb50ecfff702768a8b303ce48b86635d46be011
Fer Martin <fer@ironhack.com>
2017-11-20 14:00:36 +0000

    Improving stuff

--- templates/js/app.js
+++ templates/js/app.js
@@ -13,49 +13,24 @@ const path         = require('path');
 
 const app = express();
 
-// view engine setup
+// Middleware Setup
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Express View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+{css}
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());{css}
-app.use(express.static(path.join(__dirname, 'public')));{viewmiddleware}
-
 const index = require('./routes/index');
 app.use('/', index);
 
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = process.env.ENV === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-// Start Server
-let server = http.createServer(app);
-
-server.on('error', function (error) {
-  console.log(`Server Error: ${error}`)
-});
-
-server.listen(process.env.PORT, () => {
-  console.log(`Server Started on http://localhost:${process.env.PORT}`)
-});
+module.exports = app;
