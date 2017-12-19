2cc55165113a694972c29e1e4d510bde97338660
Marc Pomar <marc@faable.com>
2017-12-19 12:37:26 +0100

    Auth with passport, added log with debug module, persistent cookie storage with connect mongo and login & signup views

--- templates/js/app.js
+++ templates/js/app.js
@@ -8,6 +8,7 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+{auth_requires}
 
 mongoose.Promise = Promise;
 mongoose
@@ -17,6 +18,9 @@ mongoose
   }).catch(err => {
     console.error('Error connecting to mongo', err)
   });
+
+const app_name = require('./package.json').name;
+const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
 const app = express();
 
@@ -38,7 +42,10 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
+{auth}
+
 const index = require('./routes/index');
 app.use('/', index);
+{auth_routes}
 
 module.exports = app;
