623aa203ad63d85aa06f29e461fe53e277c07a5a
Fer Martin <fer@ironhack.com>
2017-12-22 11:21:43 +0000

    Passport Authentication (#4)
    * Auth with passport, added log with debug module, persistent cookie storage with connect mongo and login & signup views
    
    * small bug fix
    
    * refactor template copying
    
    * bug fixes with paths and files
    
    * - Fix handlebar conditional logic for messages
    - Added a sample helper in app.js

--- templates/js/app.js
+++ templates/js/app.js
@@ -8,6 +8,7 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+{auth_requires}
 
 mongoose.Promise = Promise;
 mongoose
@@ -17,6 +18,9 @@ mongoose
   }).catch(err => {
     console.error('Error connecting to mongo', err)
   });
+
+const app_name = require('./package.json').name;
+const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
 const app = express();
 
@@ -34,11 +38,15 @@ app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
+{auth_hbs_helper}
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
+{auth}
+
 const index = require('./routes/index');
 app.use('/', index);
+{auth_routes}
 
 module.exports = app;
